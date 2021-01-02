9c77422f04c18218d812696d18d3fdb27fa096fc
Steven Bishop <sbishopwebworks@gmail.com>
2021-01-02 17:25:35 -0500

    initial commit

---
+++ app/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="en">
+   <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Heroku</title>
+      <link rel="stylesheet" href="styles/style.css" />
+      <!--[if IE]>
+      <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+      <![endif]-->
+   </head>
+   <body>
+
+
+   <?php echo("Hello world") ?>
+       
+     
+   </body>
+   <script src="scripts/script.js"></script>
+</html>
